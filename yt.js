0bce3f0b200725f477b6908e7f83687f4fd6d186
kenrick-chan <kenrick.chan@outlook.com>
2019-02-25 16:31:40 -0500

    youtube-id-getter

---
+++ yt.js
@@ -0,0 +1,28 @@
+function YouTubeGetID(){
+	var url = document.getElementById("url");
+
+	var ID = '';
+	url = url.value.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+	if(url[2] !== undefined) {
+		ID = url[2].split(/[^0-9a-z_\-]/i);
+		ID = ID[0];
+	}
+	else {
+		ID = url;
+	}
+	//return ID;
+	alert(ID);
+}
+
+/*function YouTubeGetID(url){
+  var ID = '';
+  url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+  if(url[2] !== undefined) {
+    ID = url[2].split(/[^0-9a-z_\-]/i);
+    ID = ID[0];
+  }
+  else {
+    ID = url;
+  }
+    return ID;
+}*/
